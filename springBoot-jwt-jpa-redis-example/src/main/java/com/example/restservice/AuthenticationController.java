fe54b055b6dc0086af0730e94baf7501429cc6dd
steev <steev@laptop.lan>
2023-11-19 04:17:09 +0100

    sadad

--- springBoot-jwt-jpa-redis-example/src/main/java/com/example/restservice/AuthenticationController.java
+++ springBoot-jwt-jpa-redis-example/src/main/java/com/example/restservice/AuthenticationController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/public/authentication")
