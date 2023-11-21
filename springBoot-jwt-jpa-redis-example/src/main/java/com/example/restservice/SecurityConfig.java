1fb3099e89eef9f4fc04c51a3f74d74e8245a273
steev <steev@laptop.lan>
2023-11-21 04:00:17 +0100

    added H2 console

--- springBoot-jwt-jpa-redis-example/src/main/java/com/example/restservice/SecurityConfig.java
+++ springBoot-jwt-jpa-redis-example/src/main/java/com/example/restservice/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.restservice;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,15 +16,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import com.example.restservice.entity.UserRole;
 import com.example.restservice.repo.UserRepository;
 import com.example.restservice.service.JwtRedisService;
 
 import jakarta.servlet.DispatcherType;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
 
 @Configuration
 @EnableWebSecurity
@@ -44,21 +50,6 @@ public class SecurityConfig {
 	 AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
 	 	return authenticationConfiguration.getAuthenticationManager();
 	 }
-	 /*
-	 @Bean
-	 SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		 http.cors().and().csrf().disable();
-	     http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-	     	.and()
-	        .addFilterBefore(new AuthorizationFilter(authenticationManager(http.getSharedObject(AuthenticationConfiguration.class)), this.userRepository), UsernamePasswordAuthenticationFilter.class).authorizeRequests()
-	        //.antMatchers("/public/**").permitAll()
-	        .requestMatchers(new AntPathRequestMatcher("/public/**")).permitAll()
-	        .requestMatchers(new AntPathRequestMatcher("/private/users/resetPwd/**")).hasAnyRole("ADMIN")
-	        .expressionHandler(null)
-	        .anyRequest().authenticated();
-	    return http.build();
-	 }
-	*/
 
 	 @Bean
 	 CorsConfigurationSource  corsConfigurationSource() {
@@ -72,24 +63,31 @@ public class SecurityConfig {
 	 	source.registerCorsConfiguration("/**", configuration);
 	 	return source;
 	 }
-
+	 @Bean
+	 MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+	     return new MvcRequestMatcher.Builder(introspector);
+	 }
 	 
 	 @Bean
-	 SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	 SecurityFilterChain filterChain(HttpSecurity http,  MvcRequestMatcher.Builder mvc) throws Exception {
 		 http.cors((cors)->cors.configurationSource(corsConfigurationSource()));
 		 http.csrf((csrf) -> csrf.disable());
 		 http.sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		 
 		 http.addFilterBefore(new AuthorizationFilter(authenticationManager(http.getSharedObject(AuthenticationConfiguration.class)), this.userRepository, this.redisService), UsernamePasswordAuthenticationFilter.class);
 		 
-		 http.authorizeHttpRequests((request) -> request.dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
-				 										.requestMatchers("/public/authentication/rememberMe").authenticated()
-				 										.requestMatchers("/public/authentication/renew").authenticated()
-				 										.requestMatchers("/public/authentication/logout").authenticated()
-				 										.requestMatchers("/public/**").permitAll()
-				 										.requestMatchers("/private/users/resetpwd/**").hasAuthority(UserRole.ADMIN.name())
-				 										.requestMatchers("/private/users/**").hasAnyAuthority(UserRole.USER.name())
+		 http.headers((request) -> request.frameOptions((req) -> {req.sameOrigin();}));
+		 http.authorizeHttpRequests((request) ->request.dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
+				 										.requestMatchers(antMatcher("/h2console/**")).permitAll()
+				 										.requestMatchers(antMatcher("/public/authentication/rememberMe")).authenticated()
+				 										.requestMatchers(antMatcher("/public/authentication/renew")).authenticated()
+				 										.requestMatchers(antMatcher("/public/authentication/logout")).authenticated()
+				 										.requestMatchers(antMatcher("/public/**")).permitAll()
+				 										.requestMatchers(antMatcher("/private/users/resetpwd/**")).hasAuthority(UserRole.ADMIN.name())
+				 										.requestMatchers(antMatcher("/private/users/**")).hasAnyAuthority(UserRole.USER.name())
 				 										.anyRequest().authenticated());
 		 
 	    return http.build();
 	 } 
+	 
 }
